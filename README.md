aac3cfbc3802c18ae3b8dc9a8970736e934db8b0
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2026-06-01 01:08:15 +0700

    init app

---
+++ README.md
@@ -0,0 +1,1 @@
+A modern Jira-inspired project management platform built to help teams plan, track, and deliver work efficiently through collaborative workflows, issue tracking, and Kanban boards.
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Task Flow",
+  description:
+    "A modern Jira-inspired project management platform built to help teams plan, track, and deliver work efficiently through collaborative workflows, issue tracking, and Kanban boards.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
